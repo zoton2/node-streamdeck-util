23020cc346c12b63d0660cf341e16476c7b57e4f
zoton2 <zoton2@gmail.com>
2019-04-29 13:12:02 +0100

    Actually set up typings correctly (I hope)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,41 @@
-declare module 'ws';
+/// <reference types="node" />
+import { EventEmitter } from 'events';
+import ws from 'ws';
+declare class StreamDeck extends EventEmitter {
+    wss: ws.Server;
+    wsConnection: ws | undefined;
+    pluginUUID: string | undefined;
+    buttonLocations: object;
+    /**
+     * New instance of the streamdeck-util helper.
+     * @param opts Options object (see below).
+     * @param opts.key Secret key that will be used to connect to this server.
+     * @param opts.port Port that this server will listen on for connections.
+     * @param opts.debug Turn on debug logging to help development.
+     */
+    constructor(opts?: {
+        key?: string;
+        port?: number;
+        debug?: boolean;
+    });
+    /**
+     * Gets the buttonLocations object as received from the Stream Deck plugin.
+     */
+    getButtonLocations(): object;
+    /**
+     * Gets the pluginUUID if set.
+     */
+    getPluginUUID(): string | undefined;
+    /**
+     * Sends message to the Stream Deck WebSocket connection.
+     * as documented on https://developer.elgato.com/documentation/stream-deck/sdk/events-sent/
+     * Data will be stringified for you.
+     * @param data Object formatted to send to the Stream Deck WebSocket connection.
+     */
+    send(data: object): boolean;
+    /**
+     * Get raw WebSocket connection to the plugin backend if available.
+     */
+    getWSConnection(): ws | undefined;
+}
+export = StreamDeck;

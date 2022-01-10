2d671facf33666bf4ef2c3e8365f25f10fa0ca2c
zoton2 <zoton2@gmail.com>
2022-01-10 04:48:41 +0000

    streamdeck-plugin: remove 4.x workaround, code refactoring

--- streamdeck-plugin/com.zoton2.example.sdPlugin/pi.js
+++ streamdeck-plugin/com.zoton2.example.sdPlugin/pi.js
@@ -1,14 +1,45 @@
 var sdWS;
+var connectSocketData = {};
 var globalSettings = {};
 
-// Store data supplied by the "connectElgatoStreamDeckSocket" function.
-var connectSocketData = {
-  port: undefined,
-  pluginUUID: undefined,
-  registerEvent: undefined,
-  info: undefined,
-  actionInfo: undefined
-};
+// Initalise connection to Stream Deck's WebSocket.
+function connectToSDWS() {
+  if (sdWS) sdWS.close(); // Close current connection if one is active.
+
+  sdWS = new WebSocket(`ws://127.0.0.1:${connectSocketData.port}`);
+  console.info('Connecting to Stream Deck software');
+
+  sdWS.addEventListener('error', e => {
+    console.error('Error occured on the Stream Deck software connection:', e);
+  });
+
+  // Initalise Stream Deck WebSocket connection.
+  sdWS.addEventListener('open', () => {
+    console.info('Connection to Stream Deck software successful');
+    sdWS.send(JSON.stringify({ event: connectSocketData.registerEvent, uuid: connectSocketData.pluginUUID }));
+    sdWS.send(JSON.stringify({ event: 'getGlobalSettings', context: connectSocketData.pluginUUID }));
+  }, { once: true });
+
+  sdWS.addEventListener('close', e => {
+    console.warn('Connection to Stream Deck software closed (%s)', e.code);
+  }, { once: true });
+
+  sdWS.addEventListener('message', e => {
+    var data = JSON.parse(e.data);
+    if (data.event === 'didReceiveGlobalSettings') {
+      globalSettings = data.payload.settings;
+    }
+  });
+}
+
+// Function triggered by the popup settings window when the settings are saved.
+function gotCallbackFromWindow(data) {
+  console.info('URL/key settings have changed, saving settings');
+  globalSettings.url = data.url || 'ws://localhost:9091'; // Default if setting is empty
+  globalSettings.key = data.key || 'DEFAULT_KEY'; // Default if setting is empty
+  globalSettings.connected = false;
+  sdWS.send(JSON.stringify({ event: 'setGlobalSettings', context: connectSocketData.pluginUUID, payload: globalSettings }));
+}
 
 // Triggered by the Stream Deck software.
 function connectElgatoStreamDeckSocket(port, pluginUUID, registerEvent, info, actionInfo) {
@@ -17,56 +48,13 @@ function connectElgatoStreamDeckSocket(port, pluginUUID, registerEvent, info, ac
   connectSocketData.registerEvent = registerEvent;
   connectSocketData.info = info;
   connectSocketData.actionInfo = actionInfo;
-
   connectToSDWS();
-}
-
-// Initalise connection to Stream Deck's WebSocket.
-function connectToSDWS() {
-  if (sdWS) sdWS.close(); // Close current connection if one is active.
-
-  sdWS = new WebSocket(`ws://127.0.0.1:${connectSocketData.port}`);
-  console.info('Connecting to Stream Deck software.');
-
-  sdWS.addEventListener('error', e => {
-    console.warn('Error occured on the Stream Deck software connection: ', e);
-  });
-
-  // Initalise Stream Deck WebSocket connection.
-  sdWS.addEventListener('open', () => {
-    console.info('Connection to Stream Deck software successful.');
-    sdWS.send(JSON.stringify({ event: connectSocketData.registerEvent, uuid: connectSocketData.pluginUUID }));
-    sdWS.send(JSON.stringify({ event: 'getGlobalSettings', context: connectSocketData.pluginUUID }));
-  }, { once: true });
-
-  sdWS.addEventListener('close', e => {
-    console.warn('Connection to Stream Deck software closed (%s).', e.code);
-  }, { once: true });
-
-  sdWS.addEventListener('message', e => {
-    var data = JSON.parse(e.data);
-
-    if (data.event === 'didReceiveGlobalSettings') {
-      globalSettings = data.payload.settings;
-    }
-  });
 }
 
 // Wait for the document to fully load before doing this stuff.
 document.addEventListener('DOMContentLoaded', e => {
+  // Listen for "Open Settings Dialog" button to be clicked.
   document.getElementById('settingsButton').addEventListener('click', e => {
-    // The PI *should* receive the updated global settings when the backend updates them
-    // but when I tested it, it didn't work that way. Getting them manually while opening settings for now.
-    sdWS.send(JSON.stringify({ event: 'getGlobalSettings', context: connectSocketData.pluginUUID }));
-    setTimeout(() => window.open('settings.html'), 100);
+    window.open('settings.html');
   });
 });
-
-// Function triggered by the popup settings window when the settings are saved.
-function gotCallbackFromWindow(data) {
-  console.info('URL/key settings have changed, saving settings.');
-  globalSettings.url = data.url || 'ws://localhost:9091'; // Default if setting is empty
-  globalSettings.key = data.key || 'DEFAULT_KEY'; // Default if setting is empty
-  globalSettings.connected = false;
-  sdWS.send(JSON.stringify({ event: 'setGlobalSettings', context: connectSocketData.pluginUUID, payload: globalSettings }));
-}
